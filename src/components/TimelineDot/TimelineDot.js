9b173c14b5651f36b396c213537ce61f5f5f99af
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-20 16:17:32 +0100

    refactor components

--- src/components/TimelineDot/TimelineDot.js
+++ src/components/TimelineDot/TimelineDot.js
@@ -1,25 +1,13 @@
 import styled from "styled-components";
 import GetDayOfYearHelper from "../../services/GetDayOfYearHelper";
 import styles from "./TimelineDot.module.scss"
-// for axis
-import GetCurrentDayOfYearHelper from "@/services/GetCurrentDayOfYearHelper";
-// for today axis element
-import GetTodayFullDateHelper from "@/services/GetTodayFullDateHelper";
 
-// current day for axis
-const dayCount = GetCurrentDayOfYearHelper();
-
-// full day for axis today element
-const fullDate = GetTodayFullDateHelper();
 
 export default function TimelineDot({ id, date, color, slug }) {
   const dayOfTheYear = GetDayOfYearHelper(date);
   
   return (
     <>
-        <StyledTimelineAxis/>
-        <StyledTodayAxisElement>
-        </StyledTodayAxisElement>
         <StyledTimelineDot key={id}
         $dayoftheyear={dayOfTheYear}
         color={color} 
@@ -43,24 +31,3 @@ border-radius: 50%;
 display: inline-block;
 justify-self: center;
 `;
-
-// axis styled component
-
-
-const StyledTimelineAxis = styled.span`
-width: 1px;
-height: 200px;
-overflow: visible;
-background-color: black;
-grid-row: 1;
-grid-column: 68;
-transform: translateY(0);
-justify-self: center;
-`;
-
-// axis today element styled component
-
-const StyledTodayAxisElement = styled.p`
-    grid-column: ${dayCount};
-    transform: translateY(0);
-`
