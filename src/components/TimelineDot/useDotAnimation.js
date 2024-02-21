62bc641c3e6f46f93282f44125554cfdb489ae71
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-21 21:03:24 +0100

    add random animation

--- src/components/TimelineDot/useDotAnimation.js
+++ src/components/TimelineDot/useDotAnimation.js
@@ -1,18 +1,18 @@
 import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 
-const useGSAP = (elementRef) => {
+const useFlyInAnimation = (elementRef, index) => {
   useEffect(() => {
     const element = elementRef.current;
 
     if (element) {
       gsap.fromTo(
         element,
-        { y: -100, opacity: 0 }, // Initial position (fly from above)
-        { y: 0, opacity: 1, duration: 1, ease: 'power3.out' } // Final position (center of the screen)
+        { y: Math.random() * -1000, opacity: 0 }, // Initial position with randomness
+        { y: 0, opacity: 1, duration: 1, ease: 'power3.out', delay: index * 0.1 } // Final position with delay based on index
       );
     }
-  }, [elementRef]);
+  }, [elementRef, index]);
 };
 
-export default useGSAP;
+export default useFlyInAnimation;
