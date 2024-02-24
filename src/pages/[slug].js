83f3c25ea547531dd85b3f910ccd537475617f8d
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-24 21:28:30 +0100

    draft details page

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router"
 import useSWR from "swr"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function DateDetailsPage() {
   const router = useRouter()
@@ -15,9 +17,17 @@ export default function DateDetailsPage() {
 
   return (
     <>
-      <h1>{`${datestring}: ${title}`}</h1>
-      <h2>{author}</h2>
-      <div dangerouslySetInnerHTML={createMarkup(text)} />
+      <div className="relative bg-white h-64 m-4 p-8 border-4 rounded-3xl shadow-xl">
+        <h1 className="text-xl">{`${datestring}: ${title}`}</h1>
+        <h2 className="italic text-right pt-8">{author}</h2>
+        <div dangerouslySetInnerHTML={createMarkup(text)} />
+        <Link href="/destination-page" className="absolute top-8 right-8">
+
+          <Image src="/calenderFresh.png" alt="icon for ical data download" width={25} height={25} />
+
+        </Link>
+      </div>
+
     </>
   )
 }
