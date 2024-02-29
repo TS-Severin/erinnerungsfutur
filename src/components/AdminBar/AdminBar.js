bf671afee6c76f4c3f074d1709ff4a49de48967b
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-29 08:35:48 +0100

    style admin buttons

--- src/components/AdminBar/AdminBar.js
+++ src/components/AdminBar/AdminBar.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import useSWR from "swr";
 import EntryForm from "../EntryForm/EntryForm";
+import { useState } from "react";
 
 export default function AdminBar({ entries }) {
     const { mutate } = useSWR("/api");
+    const [isAddMode, setIsAddMode] = useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -28,16 +30,20 @@ export default function AdminBar({ entries }) {
     return (
         <>
 
-            <div className="items-center justify-between mt-4 ml-0 mr-0 p-8 bg-white border-4 rounded-3xl shadow-xl">
+            <div className="mt-4 ml-0 mr-0 p-8 bg-white border-4 rounded-3xl shadow-xl">
 
 
-                <ul className="flex space-x-4">
-                    <Link href="http://localhost:3000/api/auth/logout">Logout</Link>
+                {/* <ul className="flex space-x-4"> */}
 
-                </ul>
+                <Link className="font-bricolage mr-8 p-4 rounded-3xl shadow-md hover:bg-fuchsia-300 hover:shadow-inner active:scale-75 transition ease-in-out" href="http://localhost:3000/api/auth/logout">Logout</Link>
+
+                <button onClick={() => setIsAddMode(!isAddMode)} className="font-bricolage  p-4 rounded-3xl shadow-md hover:bg-fuchsia-300 hover:shadow-inner active:scale-75 transition ease-in-out">{isAddMode ? "Schließen" : "Neuer Eintrag"}</button>
+                {isAddMode && <EntryForm entries={entries} onHandleSubmit={handleSubmit} />}
+
+
+                {/* </ul> */}
 
             </div>
-            <EntryForm entries={entries} onHandleSubmit={handleSubmit} />
         </>
     );
 }
