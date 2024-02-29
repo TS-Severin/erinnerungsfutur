69139654e5c77bb689b34782fc79a9e71ea2622a
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-29 18:53:53 +0100

    delete scroll bar

--- src/components/TimelineDot/useDotAnimation.js
+++ src/components/TimelineDot/useDotAnimation.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import gsap from 'gsap';
 
 export default function useFlyInAnimation(elementRef, index) {
@@ -8,9 +8,16 @@ export default function useFlyInAnimation(elementRef, index) {
     if (element) {
       gsap.fromTo(
         element,
-        { y: Math.random() * -1000, opacity: 0 }, // Initial position with randomness
-        { y: 0, opacity: 1, duration: 1, ease: 'power3.out', delay: index * 0.1 } // Final position with delay based on index for each dot
+        { x: window.innerWidth, y: -100, opacity: 0, zIndex: 1000 }, // Initial position at the top right corner of the viewport with high z-index
+        { x: 0, y: 0, opacity: 1, duration: 1, ease: 'power3.out', delay: index * 0.1 } // Final position with delay based on index for each dot
       );
     }
   }, [elementRef, index]);
-};
+}
+
+
+
+
+
+
+
