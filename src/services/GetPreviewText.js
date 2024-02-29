31352f28b19ad57278b55c1d88af119fd3f82020
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-29 19:10:12 +0100

    update branch

--- src/services/GetPreviewText.js
+++ src/services/GetPreviewText.js
@@ -3,7 +3,7 @@ export default function GetPreviewText(text) {
     const words = text.split(/\s+/);
 
     // first words number
-    const previewWords = words.slice(0, 15);
+    const previewWords = words.slice(0, 10);
 
     // Join the words back into a string
     const previewText = previewWords.join(' ');
