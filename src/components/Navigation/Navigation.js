0ca8116f4836ec39254a2b6ecf2d069c87188086
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-29 16:34:29 +0100

    make mobile nav smaller

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navigation() {
 
     return (
-        <div className="flex items-center justify-between mt-4 ml-4 mr-4 p-8 bg-white border-4 rounded-3xl shadow-xl">
+        <div className="flex items-center justify-between mt-4 ml-4 mr-4 p-8 max-sm:pt-2 max-sm:pb-2 max-sm:pl-8 max-sm:pr-8 bg-white border-4 rounded-3xl shadow-xl">
 
             <Link href="/" className="font-bricolage text-4xl max-sm:text-2xl">Erinnerungsfutur</Link>
             <ul className="flex space-x-4">
