e2addb37bbec5617dd9e7873c7de33d390d935ab
Tillmann Severin <tillmann.severin@posteo.de>
2024-04-12 16:44:04 +0200

    style buttons in editor

--- src/components/Tiptap/Tiptap.js
+++ src/components/Tiptap/Tiptap.js
@@ -3,6 +3,7 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Toolbar from './Toolbar'
+import Underline from "@tiptap/extension-underline"
 import { useEffect } from 'react'
 
 const Tiptap = ({ handleTiptapText, tiptapText }) => {
@@ -16,12 +17,12 @@ const Tiptap = ({ handleTiptapText, tiptapText }) => {
 
     const editor = useEditor({
         extensions: [
-            StarterKit,
+            StarterKit, Underline
         ],
         editorProps: {
             attributes: {
                 class:
-                    "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none"
+                    "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none"
             },
 
         },
--- src/components/Tiptap/Toolbar.js
+++ src/components/Tiptap/Toolbar.js
@@ -1,5 +1,5 @@
 import {
-    Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Quote, Undo, Redo, Code,
+    Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Quote, Undo, Redo, Code, Heading,
 } from "lucide-react";
 
 
@@ -12,16 +12,15 @@ const Toolbar = ({ editor, content }) => {
     return (
         <div className="px-4 py-3 rounded-tl-md rounded-tr-md flex justify-between items-start gap-5 w-full flex-wrap border border-gray-700">
             <div className="flex justify-start items-center gap-5 w-full lg:w10/12 flex-wrap">
-
                 <button
-                    onClick={(event) => {
-                        event.preventDefault();
+                    onClick={(e) => {
+                        e.preventDefault();
                         editor.chain().focus().toggleBold().run();
                     }}
                     className={
                         editor.isActive("bold")
-                            ? "bg-sky-700 text-white p-2 rounded-lg"
-                            : "text-sky-400"
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
                     }
                 >
                     <Bold className="w-5 h-5" />
@@ -33,11 +32,24 @@ const Toolbar = ({ editor, content }) => {
                     }}
                     className={
                         editor.isActive("italic")
-                            ? "bg-sky-700 text-white p-2 rounded-lg"
-                            : "text-sky-400"
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : " p-1 rounded-lg"
                     }
                 >
                     <Italic className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleUnderline().run();
+                    }}
+                    className={
+                        editor.isActive("underline")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <Underline className="w-5 h-5" />
                 </button>
                 <button
                     onClick={(e) => {
@@ -46,14 +58,104 @@ const Toolbar = ({ editor, content }) => {
                     }}
                     className={
                         editor.isActive("strike")
-                            ? "bg-sky-700 text-white p-2 rounded-lg"
-                            : "text-sky-400"
+                            ? "fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
                     }
                 >
                     <Strikethrough className="w-5 h-5" />
                 </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleHeading({ level: 2 }).run();
+                    }}
+                    className={
+                        editor.isActive("heading", { level: 2 })
+                            ? "fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <Heading2 className="w-5 h-5" />
+                </button>
 
-
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleBulletList().run();
+                    }}
+                    className={
+                        editor.isActive("bulletList")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <List className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleOrderedList().run();
+                    }}
+                    className={
+                        editor.isActive("orderedList")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <ListOrdered className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleBlockquote().run();
+                    }}
+                    className={
+                        editor.isActive("blockquote")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <Quote className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().toggleCode().run();
+                    }}
+                    className={
+                        editor.isActive("code")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg"
+                    }
+                >
+                    <Code className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().undo().run();
+                    }}
+                    className={
+                        editor.isActive("undo")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg hover:bg-sky-700 hover:text-white p-1 hover:rounded-lg"
+                    }
+                >
+                    <Undo className="w-5 h-5" />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        editor.chain().focus().redo().run();
+                    }}
+                    className={
+                        editor.isActive("redo")
+                            ? "bg-fuchsia-300 text-white p-1 rounded-lg"
+                            : "p-1 rounded-lg hover:bg-sky-700 hover:text-white p-1 hover:rounded-lg"
+                    }
+                >
+                    <Redo className="w-5 h-5" />
+                </button>
 
             </div>
 
